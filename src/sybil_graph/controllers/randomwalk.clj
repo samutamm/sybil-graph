043d860a4970df9dea499dd3fd8bc9704ff1b410
samutamm <samutamm@gmail.com>
2017-03-25 21:21:30 +0200

    simulate multiple random walks

--- src/sybil_graph/controllers/randomwalk.clj
+++ src/sybil_graph/controllers/randomwalk.clj
@@ -16,6 +16,23 @@
                   (randomwalk/clean-visited-flags graphName)
                   {:stepsTaken i :node currentNode})))))
 
+(defn get-nodes-by-factor
+  [nodes factor]
+  (let [nodeCount (int nodes)]
+    (cond
+      (= "log" factor) (Math/round (Math/log nodeCount))
+      :else (int (* (read-string factor) nodeCount)))))
+
 (defn simulate-randomwalk
   [params]
-  ())
+  (let [nodeCount (int (read-string (:nodes params)))
+        steps (get-nodes-by-factor nodeCount (:stepfactor params))
+        seedCount (int (read-string (:seeds params)))]
+    (loop [i 0
+           resultArray []]
+        (if (<= i seedCount)
+          (recur
+            (inc i)
+            (let [results (walk-n-steps (:graphName params) (rand-int nodeCount) steps)]
+              (conj resultArray {:steps (:stepsTaken results) :node (get-in results [:node :data])})))
+          resultArray))))
